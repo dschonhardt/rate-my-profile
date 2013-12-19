a2c22dee64a0683d3094fb92dd72736ac949998c
adam terlson <adam.terlson@gmail.com>
2013-12-18 23:29:50 -0600

    Working user service

--- main.js
+++
@@ -1,9 +0,0 @@
-var scanner = require('./lib/scanner')
-scanner.open()
-	.then(function () {
-		return scanner.scan('syzdom');
-	})
-	.then(function (result) {
-		console.log(result);
-	})
-	.finally(scanner.exit);
