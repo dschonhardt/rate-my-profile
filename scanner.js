e8d56625182732d35b0d401e3e5bab48b700b928
adam terlson <adam.terlson@gmail.com>
2013-12-18 20:25:53 -0600

    Erasing the evidence

--- scanner.js
+++ scanner.js
@@ -1,7 +1,23 @@
 var phantom = require('phantom');
+
+var profileData = {
+	username: 'loribellz',
+	age: '',
+	gender: '',
+	sexualOrientation: '',
+	relationshipStatus: '',
+	city:: '',
+	state:: '',
+	photos: [],
+	tabs: [{
+		sectionName: '',
+		sectionContents: ''
+	}]
+};
+
 phantom.create(function(ph) {
 	return ph.createPage(function(page) {
-		return page.open("http://www.okcupid.com/profile/syzdom", function(status) {
+		return page.open("http://www.okcupid.com/profile/" + profileData.username, function(status) {
 			return page.evaluate(function() {
 				return document.title;
 			}, function(result) {
