01936ef766521c6d2dad0b3fe4def87a249c992e
adam terlson <adam.terlson@gmail.com>
2013-12-20 16:59:18 -0600

    User unit tests

---
+++ test/user.test.js
@@ -0,0 +1,47 @@
+var requireHijack = require('require-hijack'),
+	sites = require('../lib/sites'),
+	fakeSites = sinon.stub(sites),
+	User,
+	sandbox;
+
+requireHijack.replace('../lib/sites').with(fakeSites);
+
+User = require('../lib/user');
+
+describe('user', function () {
+	var user;
+
+	beforeEach(function () {
+		sandbox = sinon.sandbox.create();
+
+		user = new User('name');
+	});
+
+	afterEach(function () {
+		sandbox.restore();
+	});
+
+	describe('new User(username)', function () {
+		it('should create a new user with the username set to the passed username', function () {
+			// assert
+			expect(user.username).to.equal('name');
+		});
+	});
+
+	describe('getDatingProfile', function () {
+		it('should call <site>.getProfile(username)', function () {
+			// arrange
+			var datingProfile = {},
+				datingSiteName = 'okcupid';
+
+			sandbox.stub(fakeSites[datingSiteName])
+			fakeSites[datingSiteName].getProfile.withArgs(user.username).returns(Q(datingProfile));
+			
+			// act
+			return user.getDatingProfile(datingSiteName).then(function (result) {
+				expect(result).to.equal(datingProfile);
+				expect(fakeSites[datingSiteName].getProfile.calledOnce).to.be.ok;
+			});
+		});
+	});
+});
